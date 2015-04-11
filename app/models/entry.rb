097afce3ec71054c3615476e9683ddf702a6bef6
Lon Kastenson <lhkastenson@gmail.com>
2015-04-11 16:13:21 -0500

    Added more space for the entry name as per user request

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,6 @@
 class Entry < ActiveRecord::Base
-	validates :word,  presence: true, length: { maximum: 77 }
+	validates :word,  presence: true, length: { maximum: 144 }
 	validates :definition, presence: true
 	validates :word_type, length: { maximum: 77 }
-
+  
 end
